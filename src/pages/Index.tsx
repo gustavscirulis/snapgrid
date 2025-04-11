eaabe028495faec38423097fd0807839dc0bbc61
gustavscirulis <gustavs.cirulis@gmail.com>
2025-04-11 21:47:39 +0300

    Vertically center spinner

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -271,7 +271,7 @@ const Index = () => {
 
         <main className={`mx-auto flex-1 flex flex-col min-h-0 w-full ${isEmpty ? 'overflow-hidden' : ''}`}>
           {isLoading ? (
-            <div className="flex justify-center items-center h-64">
+            <div className="flex justify-center items-center h-screen">
               <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary"></div>
             </div>
           ) : (
